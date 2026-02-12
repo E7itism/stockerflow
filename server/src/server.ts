daf46dd0f52d35c3108763a021b671d4fe0ab1ae
El <eliezersgaudiel@gmail.com>
2026-02-12 18:11:32 +0800

    fix: Update CORS for production

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost',
+      'stockerflow-gxq6g5lo5-e7itisms-projects.vercel.app ',
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Routes
