5fef3d4cad40f4311ec11aefc5727998d841c280
El <eliezersgaudiel@gmail.com>
2026-03-26 13:54:33 +0800

    fix: SPA routing for refresh and remove old admin

--- admin-web-old/src-backup/components/Dashboard/Index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Dashboard Components Index
- */
-
-export { OverviewCards } from './OverviewCards';
-export { InventoryValue } from './InventoryValue';
-export { RecentActivity } from './RecentActivity';
-export { LowStockAlert } from './LowStockAlert';
-export { TopProductsChart } from './TopProductsChart';
